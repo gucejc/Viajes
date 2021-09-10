31377c9fbeb64d8eb926bcde1967b4088a947118
gutierrezcerecedo <guce_jc@yahoo.com.mx>
2021-09-10 02:43:11 -0500

    Segundo Commit -- Se agregó navegado de graficos para los fragmnetos y sus librerias , asi como los fragmentos directos

--- app/src/main/java/com/guc3/viajes/ui/MainActivity.kt
+++ app/src/main/java/com/guc3/viajes/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.guc3.viajes
+package com.guc3.viajes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.guc3.viajes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/guc3/viajes/ui/fragment/AttractionDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.guc3.viajes.ui.fragment
+
+import androidx.fragment.app.Fragment
+
+class AttractionDetailFragment : Fragment() {
+}
