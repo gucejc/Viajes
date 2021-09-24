f652c4d71cb224b043fc365e84c20f521898e0db
gutierrezcerecedo <guce_jc@yahoo.com.mx>
2021-09-24 01:57:36 -0500

    Agregamos info al fragmento detail y agregamos  boton de regreso al segundo fragmento

--- app/src/main/java/com/guc3/viajes/ui/MainActivity.kt
+++ app/src/main/java/com/guc3/viajes/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.guc3.viajes.R
 import com.guc3.viajes.data.Attraction
 import com.guc3.viajes.data.AttractionsResponse
@@ -15,6 +18,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 class MainActivity : AppCompatActivity() {
 
     lateinit var  navController:NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     val attractionsList:List<Attraction> by lazy{
         parseAttraction()
     }
@@ -25,6 +29,19 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
        navController = navHostFragment.navController
+
+        //con estas 2 lineas agregamos en la barra de navegacion el boton de regreso
+        //unicamnete en el fragmento  de detalle ojo
+        //aunque no funciona , solo es la visualizacion
+        appBarConfiguration= AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    //esta funcion sirve para activar el boton de regreso dentro de la barra de superior
+    //este boton solo se activa en el fragmento de detalle
+    override fun onSupportNavigateUp():Boolean{
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
     }
 
     private fun  parseAttraction():List<Attraction>{
--- app/src/main/java/com/guc3/viajes/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/guc3/viajes/ui/fragment/AttractionDetailFragment.kt
@@ -8,16 +8,23 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.guc3.viajes.data.Attraction
 import com.guc3.viajes.databinding.FragmentAttractionDetailBinding
 import com.guc3.viajes.databinding.FragmentHomeBinding
 import com.guc3.viajes.ui.fragment.home.HomeFragmentAdapter
 import com.guc3.viajes.ui.fragment.home.HomeFragmentDirections
+import com.squareup.picasso.Picasso
 
-class AttractionDetailFragment : Fragment() {
+class AttractionDetailFragment : BaseFragment() {
     private var _binding: FragmentAttractionDetailBinding? = null
     private val binding get() = _binding!!
 
     private val safeArgs:AttractionDetailFragmentArgs by navArgs()
+
+    private val attraction:Attraction by lazy{
+        attractions.find { it.id== safeArgs.attractionId }!! //forzamos a que no  sea nulo!!
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,7 +40,15 @@ class AttractionDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnDetail.text=safeArgs.attractionId
+        binding.tvTitileDetail.text=attraction.title
+        binding.tvDescriptionDetail.text=attraction.description
+        //todo cargando la imagen via picasso
+         Picasso.get().load(attraction.image_url).into(binding.ivHeaderDetail)
+        binding.tvVistasDetail.text=attraction.months_to_visit
+        binding.tvNumberofacts.text="${attraction.facts.size} facts"
+        binding.tvNumberofacts.setOnClickListener {
+            //todo
+        }
 
     }
 
