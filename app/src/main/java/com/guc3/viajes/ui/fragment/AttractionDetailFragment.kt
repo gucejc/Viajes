8d61dbe83644eebae54e248c1c5289fc8aef1259
gutierrezcerecedo <guce_jc@yahoo.com.mx>
2021-09-24 03:08:33 -0500

    Agregamos  el menu con el icono de maps para visualizarlo con un intent

--- app/src/main/java/com/guc3/viajes/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/guc3/viajes/ui/fragment/AttractionDetailFragment.kt
@@ -1,19 +1,21 @@
 package com.guc3.viajes.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.guc3.viajes.R
 import com.guc3.viajes.data.Attraction
 import com.guc3.viajes.databinding.FragmentAttractionDetailBinding
 import com.guc3.viajes.databinding.FragmentHomeBinding
 import com.guc3.viajes.ui.fragment.home.HomeFragmentAdapter
 import com.guc3.viajes.ui.fragment.home.HomeFragmentDirections
 import com.squareup.picasso.Picasso
+import java.util.zip.Inflater
 
 class AttractionDetailFragment : BaseFragment() {
     private var _binding: FragmentAttractionDetailBinding? = null
@@ -37,6 +39,12 @@ class AttractionDetailFragment : BaseFragment() {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //activamos el menu
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -50,6 +58,26 @@ class AttractionDetailFragment : BaseFragment() {
             //todo
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_attraction_detail,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            //este valor sale del xml de menu es el nombre  item que creamos
+            R.id.menuItemLocation ->{
+
+                val uri = Uri.parse("geo:${attraction.location.latitude},${attraction.location.longitude}?z=9&q=${attraction.title}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+                true
+            }
+            else-> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroyView() {
